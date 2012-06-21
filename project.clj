8334137b4d55a295a600730c4bc563b97e51055a
reborg <reborg@reborg.net>
2012-06-21 05:49:05 +0100

    moving this back to no implementation, moving implementations to branches

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rpn-clojure "0.1"
-  :description "An RPN-like calculator"
+(defproject enigma-clojure "0.1"
+  :description "An implementation of a 3 rotors enigma machine"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[midje "1.4.0"]])
