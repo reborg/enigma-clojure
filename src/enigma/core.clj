9e28bd8e28d52aa8911a482de1f5f00d454cf87d
reborg <reborg@reborg.net>
2012-06-26 09:13:48 +0100

    big refactoring driven by the fact I wanted to read the list of transformation in the right order. This also simplified the creation of the 26 letters map into a easy loop

--- src/enigma/core.clj
+++ src/enigma/core.clj
@@ -4,42 +4,24 @@
   ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"])
 
 (def r-rotor-wiring
-  ["a" "e" "o" "v" "f" "y" "p" "n" "z" "g" "q" "c" "m" "t" "w" "h" "l" "b" "x" "u" "r" "i" "s" "k" "d" "j"])
+  ["a" "j" "d" "k" "s" "i" "r" "u" "x" "b" "l" "h" "w" "t" "m" "c" "q" "g" "z" "n" "p" "y" "f" "v" "o" "e"])
 
 (defn shift [letters new-start]
   (let [letter-split (split-with (fn [x] (not (= x new-start))) letters)]
-    (vec (apply concat (list (last letter-split) (first letter-split))))))
+    (vec 
+      (apply concat 
+             (list 
+               (last letter-split) 
+               (first letter-split))))))
 
 (def r-rotor-map
-  {"a" (zipmap alphpabet (shift r-rotor-wiring "a"))
-   "b" (zipmap alphpabet (shift r-rotor-wiring "j"))
-   "c" (zipmap alphpabet (shift r-rotor-wiring "d"))
-   "d" (zipmap alphpabet (shift r-rotor-wiring "k"))
-   "e" (zipmap alphpabet (shift r-rotor-wiring "s"))
-   "f" (zipmap alphpabet (shift r-rotor-wiring "i"))
-   "g" (zipmap alphpabet (shift r-rotor-wiring "r"))
-   "h" (zipmap alphpabet (shift r-rotor-wiring "u"))
-   "i" (zipmap alphpabet (shift r-rotor-wiring "x"))
-   "j" (zipmap alphpabet (shift r-rotor-wiring "b"))
-   "k" (zipmap alphpabet (shift r-rotor-wiring "l"))
-   "l" (zipmap alphpabet (shift r-rotor-wiring "h"))
-   "m" (zipmap alphpabet (shift r-rotor-wiring "w"))
-   "n" (zipmap alphpabet (shift r-rotor-wiring "t"))
-   "o" (zipmap alphpabet (shift r-rotor-wiring "m"))
-   "p" (zipmap alphpabet (shift r-rotor-wiring "c"))
-   "q" (zipmap alphpabet (shift r-rotor-wiring "q"))
-   "r" (zipmap alphpabet (shift r-rotor-wiring "g"))
-   "s" (zipmap alphpabet (shift r-rotor-wiring "z"))
-   "t" (zipmap alphpabet (shift r-rotor-wiring "n"))
-   "u" (zipmap alphpabet (shift r-rotor-wiring "p"))
-   "v" (zipmap alphpabet (shift r-rotor-wiring "y"))
-   "w" (zipmap alphpabet (shift r-rotor-wiring "f"))
-   "x" (zipmap alphpabet (shift r-rotor-wiring "v"))
-   "y" (zipmap alphpabet (shift r-rotor-wiring "o"))
-   "z" (zipmap alphpabet (shift r-rotor-wiring "e"))})
+  (apply array-map 
+         (interleave 
+           alphpabet 
+           (map #(zipmap (shift alphpabet %) r-rotor-wiring) alphpabet))))
 
 (defn r-rotor [input]
-  ((r-rotor-map (input :input)) (input :offset)))
+  ((r-rotor-map (input :offset)) (input :input)))
 
 (defn enigma [input]
   (r-rotor input))
