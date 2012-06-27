2b559e46f54b7127b6538fc5cacb1b315faceea4
reborg <reborg@reborg.net>
2012-06-27 09:35:59 +0100

    first iteration: a few ideas for an high level acceptance and way to functional decomposing the problem

--- src/enigma/core.clj
+++ src/enigma/core.clj
@@ -1,1 +1,32 @@
 (ns enigma.core)
+
+(def r-rotor
+  {"b" 
+   "d" 
+   "f"
+   "h"
+   "j"
+   "l"
+   "c"
+   "p"
+   "r"
+   "t"
+   "x"
+   "v"
+   "z"
+   "n"
+   "y"
+   "e" "y"
+   "i" "l"
+   "w" "m"
+   "g" 
+   "a"
+   "k"
+   "m"
+   "u"
+   "s"
+   "q"
+   "o"})
+
+(defn enigma [start-pos input]
+  "Y")
--- test/enigma/test/test_core.clj
+++ test/enigma/test/test_core.clj
@@ -3,6 +3,9 @@
         clojure.test
         midje.sweet))
 
-(facts "enigma machine"
-       (fact "implementing the enigma machine in software is fun! (please fix me)"
-             (str "is boring") => falsey))
+(facts "about a single rotor enigma machine without rotor shifting"
+       (fact "it gives me back a Y if I say E in start position L"
+             (enigma "L" "E") => "Y")
+       (fact "it gives me back a P if I say Q in start position L"
+             (enigma "L" "Q") => "P"))
+             
