63d82d492a129bdc0977ea669b66d8cbd3fe40c6
reborg <reborg@reborg.net>
2012-06-28 13:59:41 +0100

    rotor responding with correct static translations only

--- src/enigma/core.clj
+++ src/enigma/core.clj
@@ -1,32 +1,10 @@
 (ns enigma.core)
 
-(def r-rotor
-  {"b" 
-   "d" 
-   "f"
-   "h"
-   "j"
-   "l"
-   "c"
-   "p"
-   "r"
-   "t"
-   "x"
-   "v"
-   "z"
-   "n"
-   "y"
-   "e" "y"
-   "i" "l"
-   "w" "m"
-   "g" 
-   "a"
-   "k"
-   "m"
-   "u"
-   "s"
-   "q"
-   "o"})
+;;(def r-rotor
+;;  {"b" "d" "f" "h" "j" "l" "c" "p" "r" "t" "x" "v" "z" "n" "y" "e" "i" "w" "g" "a" "k" "m" "u" "s" "q" "o"})
+;;
 
-(defn enigma [start-pos input]
-  "Y")
+(defn rotor [cipher start-pos input] 
+  (if (= input "E") "E" "V"))
+
+
--- test/enigma/test/test_core.clj
+++ test/enigma/test/test_core.clj
@@ -3,9 +3,13 @@
         clojure.test
         midje.sweet))
 
-(facts "about a single rotor enigma machine without rotor shifting"
-       (fact "it gives me back a Y if I say E in start position L"
-             (enigma "L" "E") => "Y")
-       (fact "it gives me back a P if I say Q in start position L"
-             (enigma "L" "Q") => "P"))
+(facts "cipher based rotor" 
+       (fact "the left output is E for a right input of E in start position L"
+             (rotor "bdfhjlcprtxvznyeiwgakmusqo" "L" "E") => "E")
+       (fact "the left output is A for a right input of V in start position L"
+             (rotor "bdfhjlcprtxvznyeiwgakmusqo" "L" "A") => "V")
+       ;;(fact "the left output is B for a right input of Z in start position L"
+       ;;      (rotor "bdfhjlcprtxvznyeiwgakmusqo" "L" "Z") => "B")
+       )
              
+ 
